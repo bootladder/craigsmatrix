f2afb26742d8fb85f890f1d9bf95b7d280b545a2
Steve Anderson <steve@bootladder.com>
2020-03-13 12:51:06 -0400

    don't need frontend/src/Main.elm template anymore

--- frontend/runner.sh
+++ frontend/runner.sh
@@ -1,7 +1,6 @@
 set -e
 
 printf "$(cat tests-templates/JsonDecodeTest.tmpl.elm)" "$(cat ../data/example.json)" > tests/JsonDecodeTest.elm
-printf "$(cat src-templates/Main.tmpl.elm)" "$(cat ../data/example.json)" > src/Main.elm
 elm-test
 
 elm make src/Main.elm --output main.js
